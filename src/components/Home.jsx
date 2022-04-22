de42905d5cc10c8fac115ee5d9605d49bb93b128
galenomoon <gui.galenocover@gmail.com>
2022-04-22 15:07:11 -0300

    Render Home on Screen Without react-router-dom

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Col } from "react-bootstrap"
+
+export default function Home() {
+  return (
+    <Col className="bg-light">
+      <h1>HELLO</h1>
+    </Col>
+  )
+}
